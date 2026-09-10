534964bc4eb1ad2648fdba83b74d9a4a560a3302
MaxJokar <mmb2020max@gmail.com>
2026-09-10 16:22:48 +0200

    database created, Docker created and connected

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    app_name: str = "AI Customer Agent"
+
+    environment: str = "development"
+
+    database_url: str
+
+    openai_api_key: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+
+settings = Settings()
