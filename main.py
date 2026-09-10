d5a2d26d03b16a5eb4114a3684c6fe2698163ff3
MaxJokar <mmb2020max@gmail.com>
2026-09-10 13:40:53 +0200

    .env packages added

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ def home():
     return {
         "message": "AI Agent API is running"
     }
+# Test .env use in main.py    
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+app_name = os.getenv("APP_NAME")
+
+print(app_name)
