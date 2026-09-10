7175d2f0b2f82672d80ffd18109394e5944aa9f5
MaxJokar <mmb2020max@gmail.com>
2026-09-10 16:53:29 +0200

    init_db added , fastapi creted in main.py test all is fine

--- app/main.py
+++ app/main.py
@@ -1,21 +1,57 @@
+# from fastapi import FastAPI
+
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message": "AI Agent API is running"
+#     }
+# # Test .env use in main.py    
+# from dotenv import load_dotenv
+# import os
+
+
+# load_dotenv()
+
+# app_name = os.getenv("APP_NAME")
+
+# print(app_name)
+#==============================================Test over====
+
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.core.init_db import init_db
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    await init_db()
+
+    yield
+
+
+app = FastAPI(
+    title="AI Customer Agent",
+    version="1.0.0",
+    lifespan=lifespan,
+)
 
 
 @app.get("/")
-def home():
+async def root():
+
     return {
         "message": "AI Agent API is running"
     }
-# Test .env use in main.py    
-from dotenv import load_dotenv
-import os
 
 
-load_dotenv()
 
-app_name = os.getenv("APP_NAME")
 
-print(app_name)
+
+
