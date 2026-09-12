11c88cb9dfe3c3f0cbf45e1f01ee3aa30e0a6aa3
MaxJokar <mmb2020max@gmail.com>
2026-09-12 18:18:19 +0200

    fixing database ...

--- app/core/init_db.py
+++ app/core/init_db.py
@@ -1,11 +1,29 @@
-from app.core.database import engine
-from app.models.customer import Base
+# # from app.core.database import engine
+# # from app.models.customer import Base
 
 
-async def init_db():
+# # async def init_db():
 
-    async with engine.begin() as conn:
+# #     async with engine.begin() as conn:
 
-        await conn.run_sync(
-            Base.metadata.create_all
-        )
+# #         await conn.run_sync(
+# #             Base.metadata.create_all
+# #         )
+# from app.core.database import engine
+
+
+# async def init_db():
+#     print("DATABASE: testing connection...")
+
+#     try:
+#         async with engine.begin() as conn:
+#             await conn.run_sync(lambda connection: None)
+
+#         print("DATABASE: connection successful!")
+
+#     except Exception as e:
+#         print(f"DATABASE: connection failed!")
+#         print(f"ERROR: {e}")
+#         raise
+    
+#     # to test: $ fastapi dev app/main.py
--- app/main.py
+++ app/main.py
@@ -1,40 +1,40 @@
-# from fastapi import FastAPI
+# # from fastapi import FastAPI
 
 
-# app = FastAPI()
+# # app = FastAPI()
 
 
-# @app.get("/")
-# def home():
-#     return {
-#         "message": "AI Agent API is running"
-#     }
-# # Test .env use in main.py    
-# from dotenv import load_dotenv
-# import os
+# # @app.get("/")
+# # def home():
+# #     return {
+# #         "message": "AI Agent API is running"
+# #     }
+# # # Test .env use in main.py    
+# # from dotenv import load_dotenv
+# # import os
 
 
-# load_dotenv()
+# # load_dotenv()
 
-# app_name = os.getenv("APP_NAME")
+# # app_name = os.getenv("APP_NAME")
 
-# print(app_name)
-#==============================================Test over====
+# # print(app_name)
+# #==============================================Test over====
 
 from contextlib import asynccontextmanager
-from app.api.routes.agent import router as agent_router
 
 from fastapi import FastAPI
-
 from app.core.init_db import init_db
+from app.api.routes.agent import router as agent_router
 from app.api.routes.customers import router as customer_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-
+    print("STARTUP: beginning")
     await init_db()
-
     yield
+    print("SHUTDOWN: ending")
 
 
 app = FastAPI(
@@ -42,18 +42,17 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan,
 )
+
+
 app.include_router(agent_router)
 app.include_router(customer_router)
 
+
 @app.get("/")
 async def root():
-
     return {
         "message": "AI Agent API is running"
     }
-
-
-
 
 
 
